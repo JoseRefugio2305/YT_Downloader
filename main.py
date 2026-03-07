e697cfae593db27134d9401e71b53f50a5560c0d
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2026-03-07 15:06:07 -0600

    *Movimiento a MAIN UI

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
 from PySide6.QtCore import Qt
 
 from app.database.db_manager import DBManager
+from app.ui.main_window import MainWindow
 
-class MainWindow(QMainWindow):
-     def __init__(self):
-          super().__init__()
-          self.setWindowTitle("YT Downloader"
-                              )
-          self.setMinimumSize(900,600)
+# class MainWindow(QMainWindow):
+#      def __init__(self):
+#           super().__init__()
+#           self.setWindowTitle("YT Downloader"
+#                               )
+#           self.setMinimumSize(900,600)
 
-          label = QLabel("YT Downloader")
-          label.setAlignment(Qt.AlignCenter)
-          self.setCentralWidget(label)
+#           label = QLabel("YT Downloader")
+#           label.setAlignment(Qt.AlignCenter)
+#           self.setCentralWidget(label)
 
 
 def main():

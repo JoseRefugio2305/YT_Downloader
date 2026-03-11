15d9525b5f769e3a0494ddfea89e831aec03a405
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2026-03-10 22:41:43 -0600

    *Actualizacion requirements.txt

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication
 
 from app.database.db_manager import DBManager
 from app.ui.main_window import MainWindow
-
-# class MainWindow(QMainWindow):
-#      def __init__(self):
-#           super().__init__()
-#           self.setWindowTitle("YT Downloader"
-#                               )
-#           self.setMinimumSize(900,600)
-
-#           label = QLabel("YT Downloader")
-#           label.setAlignment(Qt.AlignCenter)
-#           self.setCentralWidget(label)
-
 
 def main():
      db=DBManager("data/downloads.db")
